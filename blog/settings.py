95f2aac2635be84639c2c5aab2806c8ef60c4de8
rosekissyou <onebenbenxiong@163.com>
2018-02-07 10:15:29 +0800

    first update

--- blog/settings.py
+++ blog/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': '123',
+        'PASSWORD': 'myroot123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
